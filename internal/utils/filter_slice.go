b4611c77af123a3dc9505af5dbc19a72d435513a
alexander-yakovlev <yakovlev@ngs.ru>
2021-07-18 22:27:54 +0700

    Replaced hardcoded filter with map to exclude linear search through slice

--- internal/utils/filter_slice.go
+++ internal/utils/filter_slice.go
@@ -3,20 +3,16 @@ package utils
 //Фильтрация слайса in_slice по входному списку:
 //по критерию отсутствия элемента в фильтре in_filter (захардкожен)
 func FilterSlice(in_slice []int) ([]int, error) {
-	var in_filter = []int{1, 2, 3, 4, 5} //Захардкоженный фильтр
-
-	isFound := func(key int) bool {
-		for _, elem := range in_filter {
-			if elem == key {
-				return true
-			}
-		}
-		return false
-	}
+	var in_filter = map[int]struct{}{ //Захардкоженный фильтр
+		1: {},
+		2: {},
+		3: {},
+		4: {},
+		5: {}}
 
 	out_slice := make([]int, 0)
 	for _, elem := range in_slice {
-		if !isFound(elem) {
+		if _, found := in_filter[elem]; !found {
 			out_slice = append(out_slice, elem)
 		}
 	}
